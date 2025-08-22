b2a8d4d3f2464a9286398933755bad9ec316425c
Jaydinx7 <jaydinsolomons7@gmail.com>
2025-08-22 09:51:42 +0200

    Create adieu.py

---
+++ adieu.py
@@ -0,0 +1,13 @@
+import inflect
+p = inflect.engine()
+
+names = []
+
+try:
+    while True:
+        name = input("Name: ")
+        names.append(name)
+        
+except EOFError:
+    adieu = p.join(names)
+    print("\nAdieu, adieu, to", adieu)
